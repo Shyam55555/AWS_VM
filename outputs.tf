02eb736a9ba6e391ba95b99bf7e8ebf86acd3385
Shyam55555 <38212366+Shyam55555@users.noreply.github.com>
2026-04-02 15:22:36 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.ubuntu_rdp.public_ip
+}
+
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.ubuntu_rdp.id
+}
